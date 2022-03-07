d6d5aa019c0a97ba4fe9d59953fd0cd3cc1685aa
= <=>
2022-03-07 08:59:29 -0700

    Prototype for outputing data to web page

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const fileJson = "./WardList.json";
+const mainContainerElement = document.querySelector(".main-container");
 
-function loadWardData() {
-  fetch(fileJson)
-    .then((response) => response.text())
-    .then((data) => {
-      // Do something with your data
-      console.log(data);
-    });
+async function loadWardData() {
+  const response = await fetch(fileJson);
+
+  if (response.status >= 200 && response.status <= 299) {
+    const data = await response.json();
+    //populate html
+    loadHtml(data);
+  } else {
+    mainContainerElement.innerHTML(`Error loading file (${fileJson})`);
+  }
 }
+
+const loadHtml = (members) => {
+  members.forEach((member) => {
+    const memberElement = document.createElement("div");
+    memberElement.innerHTML = member["Preferred Name"];
+    mainContainerElement.append(memberElement);
+  });
+};
 
 loadWardData();
